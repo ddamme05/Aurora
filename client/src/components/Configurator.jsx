aee67de577f52e8a252caf314ae70e93c7a5eacf
ddamme05 <ddamme05@gmail.com>
2023-08-14 23:28:32 -0400

    Fixing href..

--- client/src/components/Configurator.jsx
+++ client/src/components/Configurator.jsx
@@ -112,8 +112,9 @@ export default function Configurator() {
         const id = response.data.id;
         // eslint-disable-next-line no-undef
         const baseUrl = process.env.NODE_ENV === 'production'
-        ? 'http://ec2-3-210-110-55.compute-1.amazonaws.com/'
+        ? 'http://ec2-3-210-110-55.compute-1.amazonaws.com'
         : 'http://localhost:5173';
+        console.log(baseUrl);
         window.location.href = `${baseUrl}/portal/${id}`;
         //navigate(`/portal/${id}`);
       } else {
