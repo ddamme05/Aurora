3fb09f3521a6a779dc6d7126a54bd177bb96114b
ddamme05 <ddamme05@gmail.com>
2023-08-14 22:58:10 -0400

    Added handling for production and development URLs.

--- client/src/components/Configurator.jsx
+++ client/src/components/Configurator.jsx
@@ -108,7 +108,11 @@ export default function Configurator() {
       // If successful, redirect to the new URL with the ID from the response
       if (response.data.success) {
         const id = response.data.id;
-        window.location.href = `http://localhost:5173/portal/${id}`;
+        // eslint-disable-next-line no-undef
+        const baseUrl = process.env.NODE_ENV === 'production'
+        ? 'https://http://ec2-54-158-220-68.compute-1.amazonaws.com'
+        : 'http://localhost:5173';
+        window.location.href = `${baseUrl}/portal/${id}`;
         //navigate(`/portal/${id}`);
       } else {
         console.error("Failed to generate quiz");
